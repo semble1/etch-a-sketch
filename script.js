495d7750929852f394ff55c31ff39ae58d52d3b6
semble1 <jaforkalam@gmail.com>
2022-04-11 20:15:21 -0400

    Added a reset button for the grid.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(".container");
+const reset = document.querySelector(".reset");
 
+//creates a 16x16 grid
 for (let i = 0; i < 256; i++) {
     const grid = document.createElement("div");
     grid.classList.add('square');
@@ -8,10 +10,18 @@ for (let i = 0; i < 256; i++) {
 
 const squares = document.querySelectorAll(".square");
 
+//changes the color of a box when it is clicked
 squares.forEach((sqr) => {
     sqr.onclick = function() {selectedSquare(sqr)};
 })
-
 function selectedSquare(sqr) {
     sqr.classList.toggle("selected");
+}
+
+//resets the grid to blank
+reset.onclick = function() {removeColors()};
+function removeColors() {
+    squares.forEach((sqr) => {
+        sqr.classList.remove("selected");
+    })
 }
