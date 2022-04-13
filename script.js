c7b94544252d5f66072bd885c8d46978d97f1d2c
semble1 <jaforkalam@gmail.com>
2022-04-12 21:14:52 -0400

    Reset button functionality, ask for new size

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 const container = document.querySelector(".container");
 const reset = document.querySelector(".reset");
+let squares = undefined;
 
 //creates a 16x16 grid
-for (let i = 0; i < 256; i++) {
-    const grid = document.createElement("div");
-    grid.classList.add('square');
-    container.appendChild(grid);
+function createGrid(size = 16) {
+    for (let i = 0; i < size*size; i++) {
+        const grid = document.createElement("div");
+        grid.classList.add('square');
+        container.appendChild(grid);
+        squares = document.querySelectorAll(".square");   
+    }
+    colorSquares();
+    container.style.gridTemplateColumns = `repeat(${size}, 0fr)`;
 }
-
-const squares = document.querySelectorAll(".square");
+createGrid();
 
 //changes the color of a box when it is clicked
-squares.forEach((sqr) => {
-    sqr.onclick = function() {selectedSquare(sqr)};
-})
+function colorSquares() {
+    squares.forEach((sqr) => {
+        sqr.onclick = function() {selectedSquare(sqr)};
+    })
+}
 function selectedSquare(sqr) {
     sqr.classList.toggle("selected");
 }
 
-//resets the grid to blank
+//resets the grid to blank and asks for size of new grid
 reset.onclick = function() {removeColors()};
 function removeColors() {
-    squares.forEach((sqr) => {
-        sqr.classList.remove("selected");
+    let size = prompt("How many squares per side?");
+    if (size > 50) {
+        size = 50;
+        alert("50 is the max!");
+    }
+    squares.forEach(sqr => {
+        sqr.remove();
     })
+    createGrid(size);
 }
